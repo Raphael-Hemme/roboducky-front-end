a56359d64e75769cb73556ca54f2a0beeadd0c36
Raphael Hemme <hemme.raphael@gmail.com>
2020-11-26 15:46:27 +0100

    Work on image sizing

--- src/components/RoboDuckyVisual.js
+++ src/components/RoboDuckyVisual.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+
 
 
 import roboduckyListeningImage from '../roboducky-patient_listener.png';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 250,
+    display: 'flex',
   },
+  image: {
+    width: 150,
+  }
 });
 
 
@@ -26,15 +25,14 @@ const RoboduckyVisual = () => {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
         <CardMedia
+          className={classes.image}
           component="img"
           alt="Roboducky is patiently listening to your struggles"
           height="230"
           image={roboduckyListeningImage}
           title="roboducky"
         />
-      </CardActionArea>
     </Card>
   );
 }
